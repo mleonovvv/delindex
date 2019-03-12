4087652e7c4f789dc4d4f2a2ce8873fbceeaefa3
Mikhail Leonov <lm@kodix.ru>
2019-03-12 12:07:11 +0300

    first commit

---
+++ curl_for_es.py
@@ -0,0 +1,38 @@
+import requests
+import re
+import datetime
+
+url = 'http://127.0.0.1:9200/_all'
+#r = requests.post(url, dict(
+#        From='+17035551212',
+#        To='+17035551212',
+#        Body='This is a test'), auth=('foo', 'bar'))
+#
+try:
+    r = requests.get(url)
+except ConnectionRefusedError:
+    print("connection error")
+
+indexList = [] # logstash-2019.02.21
+expiryDateList = []
+days = 18
+maxDate = datetime.date.today() - datetime.timedelta(days=days)
+
+for i in r.json().keys():
+    if re.search("logstash", i):
+        indexList.append(i)
+
+if len(indexList) >= days:
+
+  for l in indexList:
+      dateREGroup = re.search('([0-9]{4})\.([0-9]{2})\.([0-9]{2})', l)
+      d = datetime.date(int(dateREGroup.group(1)), int(dateREGroup.group(2)), int(dateREGroup.group(3)))
+      if d <= maxDate:
+          expiryDateList.append(d)
+
+  for i in expiryDateList:
+      s = str(i)
+      dateString = s.replace('-', '.')
+      print("logstash-" + dateString)
+else:
+    print("indexes count =< " + str(days))
