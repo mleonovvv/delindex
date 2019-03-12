b3e2a09d57b9eb590a27d267d239a7cef1b08f29
Mikhail Leonov <lm@kodix.ru>
2019-03-12 15:54:53 +0300

    кусь-кусь

--- curl_for_es.py
+++ curl_for_es.py
@@ -3,43 +3,63 @@ import re
 import datetime
 import json
 import os
+import sys
 
 
-url = 'http://127.0.0.1:9200/_all'
-try:
-    r = requests.get(url)
-except ConnectionRefusedError:
-    print("connection error")
-
-indexList = [] # logstash-2019.02.21
+indexList = []
 expiryDateList = []
 delIndex = []
-days = os.getenv('DAYS_KEEP_ES_DATA', 14)
-maxDate = datetime.date.today() - datetime.timedelta(days=int(days))
+DAYS_KEEP_ES_DATA = int(os.getenv('DAYS_KEEP_ES_DATA', 14))
+ES_PORT = int(os.getenv('ES_PORT', 9200))
+ES_HOST = os.getenv('ES_HOST', 'localhost')
+BASE_URL = '{}:{}'.format(ES_HOST, ES_PORT)
+SCHEME = 'http'
+maxDate = datetime.date.today() - datetime.timedelta(days=DAYS_KEEP_ES_DATA)
 
-for i in r.json().keys():
-    if re.search("logstash", i):
-        indexList.append(i)
 
-if len(indexList) > int(days):
+def main():
+    try:
+        url = '{}://{}/_all'.format(SCHEME, BASE_URL)
+        r = requests.get(url)
+    except requests.exceptions.ConnectionError as err:
+        print("Error: {}".format(err))
+        sys.exit(1)
 
-  for l in indexList:
-      dateREGroup = re.search('([0-9]{4})\.([0-9]{2})\.([0-9]{2})', l)
-      d = datetime.date(int(dateREGroup.group(1)), int(dateREGroup.group(2)), int(dateREGroup.group(3)))
-      if d <= maxDate:
-          expiryDateList.append(d)
+    for i in r.json().keys():
+        if 'logstash' in i:
+            indexList.append(i)
 
-  for i in expiryDateList:
-      s = str(i)
-      dateString = s.replace('-', '.')
-      delIndex.append(dateString)
-      #print("logstash-" + dateString)
-      url = 'http://127.0.0.1:9200/' + 'logstash-' + dateString
-      r = requests.get(url)
-      #print(r.json())
+    if len(indexList) > DAYS_KEEP_ES_DATA:
 
-  d = {'Found': indexList, 'Deleted': delIndex}
-  print(json.dumps(d, sort_keys=True, indent=2))
+        for l in indexList:
+            dateREGroup = re.search(r'([0-9]{4})\.([0-9]{2})\.([0-9]{2})', l)
+            d = datetime.date(
+                int(dateREGroup.group(1)),
+                int(dateREGroup.group(2)),
+                int(dateREGroup.group(3)))
+            if d <= maxDate:
+                expiryDateList.append(d)
 
-else:
-    print("indexes count < " + str(days))
+        for i in expiryDateList:
+            s = str(i)
+            dateString = s.replace('-', '.')
+            delIndex.append(dateString)
+            try:
+                url = '{}://{}/logstash-{}'.format(
+                        SCHEME,
+                        BASE_URL,
+                        dateString)
+                r = requests.get(url)
+            except requests.exceptions.ConnectionError as err:
+                print("Error: {}".format(err))
+                sys.exit(1)
+
+        d = {'Found': indexList, 'Deleted': delIndex}
+        print(json.dumps(d, sort_keys=True, indent=2))
+
+    else:
+        print("indexes count < " + str(days))
+
+
+if __name__ == '__main__':
+    main()
